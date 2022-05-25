86041010783e018ad03d2291acd763bb229cb8a0
haykam821 <24855774+haykam821@users.noreply.github.com>
2022-05-24 22:38:52 -0400

    Update Fabric API to version 0.46.1

--- src/main/java/io/github/haykam821/phantompillows/block/PhantomPillowsBlockTags.java
+++ src/main/java/io/github/haykam821/phantompillows/block/PhantomPillowsBlockTags.java
@@ -1,12 +1,12 @@
 package io.github.haykam821.phantompillows.block;
 
 import io.github.haykam821.phantompillows.Main;
-import net.fabricmc.fabric.api.tag.TagRegistry;
+import net.fabricmc.fabric.api.tag.TagFactory;
 import net.minecraft.block.Block;
 import net.minecraft.tag.Tag;
 import net.minecraft.util.Identifier;
 
 public class PhantomPillowsBlockTags {
 	private static final Identifier ELYTRA_CUSHIONING_BLOCKS_ID = new Identifier(Main.MOD_ID, "elytra_cushioning_blocks");
-	public static final Tag<Block> ELYTRA_CUSHIONING_BLOCKS = TagRegistry.block(ELYTRA_CUSHIONING_BLOCKS_ID);
+	public static final Tag<Block> ELYTRA_CUSHIONING_BLOCKS = TagFactory.BLOCK.create(ELYTRA_CUSHIONING_BLOCKS_ID);
 }
